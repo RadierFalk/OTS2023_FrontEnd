4d6797b731e6fb17ef30fd2d5f36fe2c68eb91ed
Radier Falcao <93263292+RadierFalk@users.noreply.github.com>
2023-09-25 20:57:18 -0400

    Criando itens

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
+  const item1 = {
+    nome: 'Rick Sanchez',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+
+  const item2 = {
+    nome: 'Morty Smith',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
 
   return (
     <>
-    <Card />
+      <Card item={item1} />
+      <Card item={item2} />
     </>
   )
 }
