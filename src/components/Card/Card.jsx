c186e95ce2801666cb267d648e4ae9a138589576
Radier Falcao <93263292+RadierFalk@users.noreply.github.com>
2023-09-27 21:27:31 -0400

    Correção Tags

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,22 +2,16 @@ import Tag from "../Tag/Tag";
 import "./Card.css"
 
 export default function Card(props) {
-  const item = props.item;
+  const item = props.item
 
-  // Garantir que temos sempre um array em tags
-
-  // Jeito literal
-  // let tags = item.tags;
-
-  // if (!tags) {
-  //   tags = [];
-  // }
-
-  // Jeito otimizado
-  const tags = item.tags || [];
+  const tags = [
+    `Status: ${item.status}`,
+    `Species: ${item.species}`,
+    `Origin: ${item.origin.name}`,
+  ]
 
   return <div className="card">
-    <h2>{item.nome}</h2>
+    <h2>{item.name}</h2>
 
     <div className="tags">
       {tags.map(function (tag, index) {
@@ -25,6 +19,6 @@ export default function Card(props) {
       })}
     </div>
 
-    <img src={item.imagemUrl} />
+    <img src={item.image} />
   </div>
 }
