e785452d34a34aa5c70cc5a70c001c95ce358cc3
Radier Falcao <93263292+RadierFalk@users.noreply.github.com>
2023-09-27 21:09:55 -0400

    Update Tags

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,30 +1,17 @@
-import Tag from "../Tag/Tag";
-import "./Card.css"
+import './Tag.css'
 
-export default function Card(props) {
-  const item = props.item;
+export default function Tag(props) {
+    /*
+    Conteúdo do componente
+    */
 
-  // Garantir que temos sempre um array em tags
+    const text = props.text;
 
-  // Jeito literal
-  // let tags = item.tags;
+    /*
+    Return -> Encerra a função
+    No ReactJS, o return precisa devolver um componente JSX para ser
+    exibido na tela
+    */
 
-  // if (!tags) {
-  //   tags = [];
-  // }
-
-  // Jeito otimizado
-  const tags = item.tags || [];
-
-  return <div className="card">
-    <h2>{item.name}</h2>
-
-    <div className="tags">
-      {tags.map(function (tag, index) {
-        return <Tag text={tag} key={`card_tag_${index}`} />
-      })}
-    </div>
-
-    <img src={item.image} />
-  </div>
+    return <div className="tag">{text}</div>
 }
